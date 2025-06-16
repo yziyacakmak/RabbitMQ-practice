bc9ef49e5b01ef904baa31b4d051874aad82d586
yusuf.cakmak <yusuf.cakmak@saykal>
2025-06-16 16:03:25 +0300

    hello world

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,28 @@
-﻿
+﻿using RabbitMQ.Client;
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+var queueName = "hello-queue";
+await channel.QueueDeclareAsync(queueName, durable: true, exclusive: false, autoDelete: false);
+
+var message = "Hello, RabbitMQ!";
+var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(
+    exchange: string.Empty,
+    routingKey: queueName,
+    mandatory: false,
+    body: body
+);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
 
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+var queueName = "hello-queue";
+//await channel.QueueDeclareAsync(queueName, durable: true, exclusive: false, autoDelete: false);
+
+var consumer=new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    await Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer
+);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
