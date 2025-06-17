36c74adad67aec62390976ed48c841866f58041d
yusuf.cakmak <yusuf.cakmak@saykal>
2025-06-17 09:06:16 +0300

    work queue

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -11,18 +11,19 @@ using var channel = await connection.CreateChannelAsync();
 var queueName = "hello-queue";
 await channel.QueueDeclareAsync(queueName, durable: true, exclusive: false, autoDelete: false);
 
-var message = "Hello, RabbitMQ!";
-var body = System.Text.Encoding.UTF8.GetBytes(message);
+Enumerable.Range(1, 50).ToList().ForEach(async i =>
+{
+    var message = $"Message {i}";
+    var body = System.Text.Encoding.UTF8.GetBytes(message);
 
-await channel.BasicPublishAsync(
+    await channel.BasicPublishAsync(
     exchange: string.Empty,
     routingKey: queueName,
     mandatory: false,
-    body: body
-);
+    body: body);
+    Console.WriteLine($" [x] Sent {message}");
 
-Console.WriteLine($" [x] Sent {message}");
-
+});
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
 
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -9,6 +9,12 @@ using var connection = await factory.CreateConnectionAsync();
 
 using var channel = await connection.CreateChannelAsync();
 
+await channel.BasicQosAsync(
+    prefetchSize: 0,
+    prefetchCount: 1,
+    global: false
+);
+
 var queueName = "hello-queue";
 //await channel.QueueDeclareAsync(queueName, durable: true, exclusive: false, autoDelete: false);
 
@@ -16,9 +22,14 @@ var consumer=new AsyncEventingBasicConsumer(channel);
 
 consumer.ReceivedAsync += async (model, ea) =>
 {
+    await Task.Delay(1000); // Simulate some processing delay
     var body = ea.Body.ToArray();
     var message = System.Text.Encoding.UTF8.GetString(body);
     Console.WriteLine($" [x] Received {message}");
+    await channel.BasicAckAsync(
+        deliveryTag: ea.DeliveryTag,
+        multiple: false
+    );
     await Task.CompletedTask;
 };
 
